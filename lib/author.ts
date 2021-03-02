f2c06b8e857b5aecf40c38bab62d3dfe5833c6e7
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-02 16:57:00 +0900

    libに分けてみる

---
+++ lib/author.ts
@@ -0,0 +1,44 @@
+import { GraphQLClient } from 'graphql-request'
+const { GRAPH_CMS_API } = process.env
+
+export type PictureProp = {
+  id: string
+  url: string
+}
+
+export type AuthorProp = {
+  name: string
+  picture: PictureProp
+  job: string
+  biography?: string | null | undefined
+}
+
+const author = async (): Promise<{
+  props: {
+    authors: [AuthorProp]
+  }
+}> => {
+  const graphcms = new GraphQLClient(GRAPH_CMS_API)
+  const { authors } = await graphcms.request(
+    `
+      { 
+        authors {
+          id
+          name
+          picture{
+            id
+            url
+          }
+        } 
+      }
+    `
+  )
+
+  return {
+    props: {
+      authors
+    }
+  }
+}
+
+export { author }
