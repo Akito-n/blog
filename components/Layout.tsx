132cc56ed008df72eebc1df8069c20102378892c
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-02 18:28:14 +0900

    layout

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import styles from '../styles/layout.module.css'
+import Link from 'next/link'
+
+export const Layout = ({
+  children,
+  home
+}: {
+  children: any
+  home: boolean
+}): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
