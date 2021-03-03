589750e83fd5635efe42f5de340a6ece4f5de6e6
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-03 12:23:43 +0900

    ルートに戻れる

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 export const Header = (): JSX.Element => {
   return (
     <nav className="flex flex-wrap items-center justify-between p-6 bg-primary-dark bg-teal">
       <div className="flex items-center mr-6 text-white flex-no-shrink">
         <span className="text-xl font-semibold tracking-tight">
-          Saboten Blog
+          <Link href="/">
+            <a className="text-white no-underline">Saboten Blog</a>
+          </Link>
         </span>
       </div>
     </nav>
