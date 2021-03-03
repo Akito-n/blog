ae918ed1794ff6b8e7b4a8ebe2776e19a8481801
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-03 12:38:21 +0900

    add css

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const Header = (): JSX.Element => {
   return (
-    <nav className="flex flex-wrap items-center justify-between p-6 bg-primary-dark bg-teal">
+    <nav className="flex flex-wrap items-center justify-between p-6 mb-10 bg-primary-dark bg-teal">
       <div className="flex items-center mr-6 text-white flex-no-shrink">
         <span className="text-xl font-semibold tracking-tight">
           <Link href="/">
