30698c84976198ef9ae1dc69f6d6c0ba4b612224
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-04 16:57:57 +0900

    かなりいい感じのoffset

--- components/Topic.tsx
+++ components/Topic.tsx
@@ -1,3 +1,4 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 export type Toc = {
   level: number
   slug: string
@@ -7,9 +8,13 @@ const Topic = ({ toc }: { toc: Toc | undefined | null }): JSX.Element => {
   if (!toc) return <></>
 
   const link = (
-    <a className="block p-3 no-underline" href={`#${toc.slug}`}>
+    <AnchorLink
+      className="block p-2 text-black no-underline hover:bg-gray-200"
+      href={`#${toc.slug}`}
+      offset={80}
+    >
       {toc.title}
-    </a>
+    </AnchorLink>
   )
   let topic = <></>
   switch (toc.level) {
@@ -18,7 +23,14 @@ const Topic = ({ toc }: { toc: Toc | undefined | null }): JSX.Element => {
       break
     case 2:
       topic = (
-        <ul className="p-0 list-none">
+        <ul className="list-none">
+          <li>{link}</li>
+        </ul>
+      )
+      break
+    case 3:
+      topic = (
+        <ul className="ml-5 list-none">
           <li>{link}</li>
         </ul>
       )
