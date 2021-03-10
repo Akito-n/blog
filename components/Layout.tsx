bb9c8e19b30d8ea7c7eb65c2c39ba07431e22e38
Akito-n <a.nagai@mof-mof.co.jp>
2021-03-10 15:54:52 +0900

    footer作るまで一旦フルスクリーン

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 export const Layout = ({ children }: { children: any }): JSX.Element => {
   return (
-    <div className="pt-20 bg-gray-100">
+    <div className="h-screen pt-20 bg-gray-100">
       <main className="block">{children}</main>
     </div>
   )
